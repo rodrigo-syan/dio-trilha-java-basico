b542f2bdb84404161b4e43206a4967b27b9645b0
rodrigo-syan <rodrigosyan@gmail.com>
2022-10-24 17:18:39 -0300

    Commit

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!");
+        double salarioMinimo = 2500;
+        byte idade = 123;
+        short ano = 2022;
+        int cep = 60861634;
+        long cpf = 1234567890L;
+        float pi = 3.14F;
+        String cpfZero = "01234567890";
+
+        System.out.println(salarioMinimo);
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(cpfZero);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //cast
+
     }
 }
